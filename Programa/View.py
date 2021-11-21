0696a371e175452d48c1946a9caf6fb3fda3ef6d
Luis Gustavo <83785864+Gransbi@users.noreply.github.com>
2021-11-21 12:46:01 -0300

    Update View.py

---
+++ Programa/View.py
@@ -0,0 +1,39 @@
+
+# Barra superior 
+# 		1-Home
+# 		2-Animes
+# 		3-Filmes 
+# 		4-Fila
+# 		5-Pesquisa
+# 		6-login
+# 
+# 	Home
+# 		7-Em alta
+# 		8-Ultimo adicionados
+# 	Animes
+# 		7-Lista de Generos
+# 		8...Animes por ordem alfabetica
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+
+
+
+
+
