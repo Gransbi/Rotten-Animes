14705c6fd33c5f4310431aee4c05a36fe3ae8950
Luis Gustavo <83785864+Gransbi@users.noreply.github.com>
2021-11-22 15:20:08 -0300

    ajustes usuario

---
+++ Programa/usuario.py
@@ -0,0 +1,12 @@
+class Usurio:
+
+
+	def __init__(self,login,senha,apelido,adiministrador = False):
+		self.login = login
+		self.senha = senha
+		self.apelido = apelido
+		self.adiministrador = adiministrador
+
+        
+    	def conectar(self):
+        	pass
