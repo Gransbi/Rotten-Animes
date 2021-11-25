6b8e55936a855824ae89fa68b6f9ad4039c9075a
Luis Gustavo <83785864+Gransbi@users.noreply.github.com>
2021-11-25 11:57:59 -0300

    episodeis

--- Programa/controller.py
+++ Programa/controller.py
@@ -1,37 +1,52 @@
 class Anime:
 
 
-    def __init__(self, status, Qt_ep, autor, relacionados, resenha, temporadas, avaliacao = 0, avaliacao_list = []):
-        self.status = status
-        self.Qt_ep = Qt_ep
-        self.autor = autor
-        self.relacionados = relacionados
-        self.resenha = resenha
-        self.temporadas = temporadas
-        self.avaliacao = avaliacao
-        self.avaliacao_list = avaliacao_list
+	def __init__(self, nome,status, Qt_ep, autor, relacionados, resenha, temporadas, avaliacao = 0, avaliacao_list = []):
+		self.nome = nome
+		self.status = status
+		self.Qt_ep = Qt_ep
+		self.autor = autor
+		self.resenha = resenha
+		self.temporadas = temporadas
+		self.avaliacao = avaliacao
+		self.avaliacao_list = avaliacao_list
 
 
-        def avaliar(self, avaliacao):
-                self.avaliacao_list.append(avaliacao)
-                self.avaliacao = (sum(avaliacao_list) / len(avaliacao_list))
-        
+	def avaliar(self, avaliacao):
+		self.avaliacao_list.append(avaliacao)
+		self.avaliacao = (sum(self.avaliacao_list) / len(self.avaliacao_list))
+	
 
-        def acesso(self, status_usuario):
-                self.status_usuario = status_usuario
-                if status_usuario == True:
-                   print("Adminitrador")
-                else:
-                        print("mambro comum")
+	def acesso(self, status_usuario):
+		self.status_usuario = status_usuario
+		if status_usuario == True:
+		   print("Adminitrador")
+		else:
+			print("mambro comum")
     
 
-        def print_resenha(self, opção):
-                if self.print_resenha:
-                        print(self.resenha)
+	def print_resenha(self, opção):
+		if self.print_resenha:
+			print(self.resenha)
 
-        def mudar(self):
-                if (Usurio.validar("adiministrador") == True):
-                        pass
+	def editar_anime(self):
+		if (Usurio.validar("adiministrador") == True):
+			pass
+
+
+class Episodeo:
+
+	def __init__(self, nome_anime, nome_ep, num_ep,cannon,resenha):
+		self.nome_anime = nome_anime
+		self.nome_ep = nome_ep
+		self.num_ep = num_ep
+		self.cannon = cannon
+		self.resenha = resenha
+
+
+	def editar_ep(self):
+		if (Usurio.validar("adiministrador") == True):
+			pass
 
 
 class Usurio:
@@ -45,46 +60,46 @@ class Usurio:
 		self.coneccao = coneccao
 
 
-        def conectar_on(self,login,senha,banco):
-                if(login in banco.itens()):
-                        if (banco.get(login) == senha):
-                                print("Você logou com sucesso.")
-                                self.coneccao == True
-                                return True
+	def conectar_on(self,login,senha,banco):
+		if(login in banco.itens()):
+			if (banco.get(login) == senha):
+				print("Você logou com sucesso.")
+				self.coneccao == True
+				return True
 
-                        else:
-                                print("Sua senha esta invalida.")
-                else:
-                        print("Seu login não existe.")
-                        return False
+			else:
+				print("Sua senha esta invalida.")
+		else:
+			print("Seu login não existe.")
+			return False
 
-        def validar(self, escolha):
-                if (escolha == "coneccao"):
-                        return self.coneccao
-                elif (escolha == "adiministrador"):
-                        return self.adiministrador
+	def validar(self, escolha):
+		if (escolha == "coneccao"):
+			return self.coneccao
+		elif (escolha == "adiministrador"):
+			return self.adiministrador
 
 
 class Tratamento:
-        def entrada(valor, parametro_1, parametro_2):
-                if ((valor.isalnum() == True) and (valor.isalpha() == False)):
-                        if (int(valor) >= parametro_1 and int(valor) <= parametro_2):
-                                print("Você informol um valor invalido")
-                                return True
+	def entrada(valor, parametro_1, parametro_2):
+		if ((valor.isalnum() == True) and (valor.isalpha() == False)):
+			if (int(valor) >= parametro_1 and int(valor) <= parametro_2):
+				print("Você informol um valor invalido")
+				return True
 
 
-                else:
-                        return False
-                
-        def avaliacao_(valor, parametro_1, parametro_2):
-                if ((valor.isalnum() == True) and (valor.isalpha() == False)):
-                        if (int(valor) >= parametro_1 and int(valor) <= parametro_2):
-                                print("Você informol um valor invalido")
-                                return True
+		else:
+			return False
+		
+	def avaliacao_(valor, parametro_1, parametro_2):
+		if ((valor.isalnum() == True) and (valor.isalpha() == False)):
+			if (int(valor) >= parametro_1 and int(valor) <= parametro_2):
+				print("Você informol um valor invalido")
+				return True
 
 
-                else:
-                        return False
+		else:
+			return False
 
 
 
