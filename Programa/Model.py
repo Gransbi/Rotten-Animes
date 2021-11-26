e3777fdac66de015bfee738809bb42991cee610b
Luis Gustavo <83785864+Gransbi@users.noreply.github.com>
2021-11-26 10:33:37 -0300

    de novo

---
+++ Programa/Model.py
@@ -0,0 +1,23 @@
+class Cadastrar:
+
+
+	def anime():
+		pass
+
+
+	def episodeo():
+		pass
+
+
+	def usuario():
+		pass
+
+class Leitura:
+
+
+	def animes_de_dados():
+		arquivo = open("animes.csv", "r")
+		for line in arquivo:
+			pass
+
+		arquivo.close()
